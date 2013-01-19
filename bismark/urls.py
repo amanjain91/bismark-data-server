1392928e4966dce5bfded87a0c5d2d9de38fb714
root <root@ip-10-242-231-41.ec2.internal>
2013-01-19 20:40:36 +0000

    Changed project structure according to new Django specification

--- bismark/urls.py
+++ bismark/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    (r'^upload/', 'data_server.views.upload'),
+    (r'^upload/', 'bismark.views.upload'),
 )
---
+++ cron/rsync_to_dp4.sh
@@ -0,0 +1,1 @@
+rsync -avz -e "ssh -i /srv/keys/dp4-key" /srv/www/bismark/bismark_data/ bismark-data-xfer@dp4.gtnoise.net:/home/bismark/var/data/http_uploads/
