2ea008eee9d9a96d7789087ffef00f6a7d141ce8
root <root@ip-10-202-249-78.ec2.internal>
2013-02-23 18:05:51 +0000

    move tar files that have been taken care off to /bismark_data_backup

--- scripts/data_untar.sh
+++ scripts/data_untar.sh
@@ -1,7 +1,14 @@
 #! /bin/bash
-# untars all files in ~/bismark_data_from_s3 and puts them in ~/bismark_data_untared
+
+# untars all files in ~/bismark_data_from_s3 and puts them in ~/bismark_data_untared.
+# also moves the .tar versions to ~/bismark_data_backup
 
 for tar_file in $(find ~/bismark_data_from_s3 -name *.tar -mmin +5)
 do
 	tar -xvf $tar_file -C ~/bismark_data_untared/
+	base_filename=$(basename $tar_file)
+	experiment_name=$(echo $base_filename | cut -d'_' -f1)
+	device_name=$(echo $base_filename | cut -d'_' -f2)
+	mkdir -p ~/bismark_data_backup/$experiment_name/$device_name/
+	mv $tar_file ~/bismark_data_backup/$experiment_name/$device_name/
 done
