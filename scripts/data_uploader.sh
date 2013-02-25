691db79c3d6a6614527cd8b3af5d2a10361ab6e7
Srikanth Sundaresan <srikanth@gatech.edu>
2013-02-25 13:38:39 -0500

    mv to organized data directory in EBS

--- scripts/data_uploader.sh
+++ scripts/data_uploader.sh
@@ -6,6 +6,7 @@ set -e
 #This script uploads all files in /bismark_data/outbox to s3 and also moves the files to /bismark_data/uploaded
 #runs on ec2
 
+ebs_dir=/bismark_data/uploaded
 find /bismark_data/outbox -name "*.tar.gz" -mmin +5 | while read -r file; do
 	base_filename=$(basename $file)
 	experiment_name=$(echo $base_filename | cut -d'_' -f1)
@@ -14,5 +15,6 @@ find /bismark_data/outbox -name "*.tar.gz" -mmin +5 | while read -r file; do
 	from=$file
 	to=s3://bismark_data/tarballs/$experiment_name/$device_name/$datestamp/$base_filename
 	s3cmd -v put $from $to
-	mv $file /bismark_data/uploaded/$base_filename
+	mkdir -p $ebs_dir/$experiment_name/$device_name/$datestamp
+	mv $file $ebs_dir/$experiment_name/$device_name/$datestamp/$base_filename
 done
