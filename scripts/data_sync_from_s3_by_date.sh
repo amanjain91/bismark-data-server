760d6f49cc42562447dd7328c082e1be0b338c77
Aman Jain <amanjain@gatech.edu>
2013-09-05 03:16:21 +0000

    adding a / at the end of dir path for sync to work properly

--- scripts/data_sync_from_s3_by_date.sh
+++ scripts/data_sync_from_s3_by_date.sh
@@ -18,8 +18,10 @@ flock -n 200 || exit 1
 # Rest of syncing code below
 todays_date=$(date '+%Y%m%d')
 yesterdays_date=$(date --date="yesterday" '+%Y%m%d')
-root_dir='bismark_data_from_s3_by_date'
+root_dir=(~/bismark_data_from_s3_by_date)
 
 # Sync yesterdays folder for redundancy
-s3cmd sync --skip-existing s3://bismark_data/tarballs_by_date/$yesterdays_date ~/$root_dir/$yesterdays_date
-s3cmd sync --skip-existing s3://bismark_data/tarballs_by_date/$todays_date ~/$root_dir/$todays_date
+mkdir -p $root_dir/$yesterdays_date
+s3cmd sync --skip-existing s3://bismark_data/tarballs_by_date/$yesterdays_date/ $root_dir/$yesterdays_date/
+mkdir -p $root_dir/$todays_date
+s3cmd sync --skip-existing s3://bismark_data/tarballs_by_date/$todays_date/ $root_dir/$todays_date/
