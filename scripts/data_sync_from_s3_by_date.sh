c53f8f1ac4e3412ce3fde3d298fcfa0d2b5754fb
Aman Jain <amanjain@gatech.edu>
2013-09-05 02:43:16 +0000

    changes to the new script file

--- scripts/data_sync_from_s3_by_date.sh
+++ scripts/data_sync_from_s3_by_date.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+# Runs on dp4
+# Syncs bismark data from s3 to dp4 for today and yesterday
+
 # Exit on first error
 set -e
 
-#runs on dp3
-#syncs bismark data from s3 to dp4 for today and yesterday
+lock=(~/s3sync_by_date.lockfile)
 
+# Open lockfile and name its file handle 200 (number chosen arbitarily)
+exec 200>$lock
+
+# If lock is already taken then exit
+flock -n 200 || exit 1
+
+# Rest of syncing code below
 todays_date=$(date '+%Y%m%d')
 yesterdays_date=$(date --date="yesterday" '+%Y%m%d')
 root_dir='bismark_data_from_s3_by_date'
 
-s3cmd sync --skip-existing s3://bismark_data/tarballs_by_date/$yesterdays_date ~/$root_dir
-s3cmd sync --skip-existing s3://bismark_data/tarballs_by_date/$todays_date ~/$root_dir
+# Sync yesterdays folder for redundancy
+s3cmd sync --skip-existing s3://bismark_data/tarballs_by_date/$yesterdays_date ~/$root_dir/$yesterdays_date
+s3cmd sync --skip-existing s3://bismark_data/tarballs_by_date/$todays_date ~/$root_dir/$todays_date
+~
+
